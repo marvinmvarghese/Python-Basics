50d0b7d941e48b5543d305c1c9724cb125a4cb90
Marvin <12marvinmv@gmail.com>
2025-12-04 12:27:51 +1100

    hi

---
+++ task2.py
@@ -0,0 +1,3 @@
+x = int(input('enter a number'))
+for x in range(0,x):
+    print(x*x)
---
+++ task3.py
@@ -0,0 +1,5 @@
+x = input("enter the string")
+for i in x:
+    if i.lower() not in "app":
+
+        print(i, end="")
